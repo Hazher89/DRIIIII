2279e28668c968c1ea7fcb75505cd52d2bc552ee
hazher <baxigshti@hotmail.de>
2026-03-16 22:06:59 +0100

    chore: Oppdaterer build script med pub get og precache

--- build_web.sh
+++ build_web.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Klargjør Flutter SDK..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
+./flutter/bin/flutter precache --web
+./flutter/bin/flutter pub get
 ./flutter/bin/flutter build web --release --web-renderer canvaskit
 echo "Bygg ferdig!"
